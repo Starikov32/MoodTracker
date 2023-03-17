71fc1b60462f75d398c7242eadb386705cbb1f54
Starikov32 <hssurname@gmail.com>
2023-03-17 21:17:46 +0300

    Fix gradle files

--- app/src/main/java/com/example/moodtracker/MainActivity.kt
+++ app/src/main/java/com/example/moodtracker/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             MoodTrackerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("Android2")
                 }
             }
         }

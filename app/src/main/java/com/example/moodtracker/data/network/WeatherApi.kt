0541073989a64bdda3ada079002a3202e13f3c75
Starikov32 <hssurname@gmail.com>
2023-03-22 01:16:16 +0300

    add ktlint and format

--- app/src/main/java/com/example/moodtracker/data/network/WeatherApi.kt
+++ app/src/main/java/com/example/moodtracker/data/network/WeatherApi.kt
@@ -7,6 +7,6 @@ interface WeatherApi {
     @GET("v1/forecast?hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m,pressure_msl")
     suspend fun getWeatherData(
         @Query("latitude") lat: Double,
-        @Query("longitude") long: Double
+        @Query("longitude") long: Double,
     ): WeatherDto
 }

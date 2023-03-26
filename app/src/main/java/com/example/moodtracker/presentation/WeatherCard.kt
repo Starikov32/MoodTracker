749d20f32a2129361556a1a0865f39aa7e88bde4
Ikrom <nacud85@gmail.com>
2023-03-26 22:24:59 +0300

    added all of the weather part

--- app/src/main/java/com/example/moodtracker/presentation/WeatherCard.kt
+++ app/src/main/java/com/example/moodtracker/presentation/WeatherCard.kt
@@ -3,22 +3,17 @@ package com.example.moodtracker.presentation
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
@@ -37,7 +32,7 @@ fun WeatherCard(
 ) {
     state.weatherInfo?.currentWeatherData?.let { data ->
         Card(
-            backgroundColor = backgroundColor,
+            colors = CardDefaults.cardColors(containerColor = backgroundColor),
             shape = RoundedCornerShape(10.dp),
             modifier = modifier.padding(16.dp),
         ) {

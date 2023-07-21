60370aa49d49ff95567e148fc9eae2ee8081dadb
Moriarty <nacud85@gmail.com>
2023-07-21 19:25:25 +0300

    fix Constants

--- app/src/main/java/com/radzhabov/moodtracker/data/network/api/WeatherApi.kt
+++ app/src/main/java/com/radzhabov/moodtracker/data/network/api/WeatherApi.kt
@@ -2,15 +2,15 @@ package com.radzhabov.moodtracker.data.network.api
 
 import com.radzhabov.moodtracker.data.network.dto.WeatherDto
 import com.radzhabov.moodtracker.domain.util.Constants.Companion.WEATHER_ENDPOINT
-import com.radzhabov.moodtracker.domain.util.Constants.Companion.latitude
-import com.radzhabov.moodtracker.domain.util.Constants.Companion.longitude
+import com.radzhabov.moodtracker.domain.util.Constants.Companion.LATITUDE
+import com.radzhabov.moodtracker.domain.util.Constants.Companion.LONGITUDE
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherApi {
     @GET(WEATHER_ENDPOINT)
     suspend fun getWeatherData(
-        @Query(latitude) lat: Double,
-        @Query(longitude) long: Double,
+        @Query(LATITUDE) lat: Double,
+        @Query(LONGITUDE) long: Double,
     ): WeatherDto
 }
--- app/src/main/java/com/radzhabov/moodtracker/domain/util/Constants.kt
+++ app/src/main/java/com/radzhabov/moodtracker/domain/util/Constants.kt
@@ -5,7 +5,7 @@ class Constants {
         const val URL_API = "https://api.open-meteo.com/"
         const val WEATHER_ENDPOINT = "v1/forecast?hourly=temperature_2m," +
                 "weathercode,relativehumidity_2m,windspeed_10m,pressure_msl"
-        const val latitude = "latitude"
-        const val longitude = "longitude"
+        const val LATITUDE = "latitude"
+        const val LONGITUDE = "longitude"
     }
 }

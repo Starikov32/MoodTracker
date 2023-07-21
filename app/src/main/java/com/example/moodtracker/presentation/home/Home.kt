5f58b02e74253b79db6bc1654593d542ffd338a8
IkromR <nacud85@gmail.com>
2023-07-21 13:57:55 +0300

    moved the folder

--- app/src/main/java/com/example/moodtracker/presentation/home/Home.kt
+++ app/src/main/java/com/example/moodtracker/presentation/home/Home.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
-
 @Composable
 fun Home(
     padding: PaddingValues,
--- app/src/main/java/com/example/moodtracker/presentation/stats/Stats.kt
+++ app/src/main/java/com/example/moodtracker/presentation/stats/Stats.kt
@@ -1,12 +1,9 @@
 package com.example.moodtracker.presentation.stats
 
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
-fun Stats(
-    padding: PaddingValues
-) {
+fun Stats() {
     Text(text = "Stats screen")
 }

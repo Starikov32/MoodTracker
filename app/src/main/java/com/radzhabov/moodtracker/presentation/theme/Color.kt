bf705050d2e3714da0a867c4e71960c098c2b57f
Moriarty <nacud85@gmail.com>
2023-07-24 17:22:02 +0300

    added home content card

--- app/src/main/java/com/radzhabov/moodtracker/presentation/theme/Color.kt
+++ app/src/main/java/com/radzhabov/moodtracker/presentation/theme/Color.kt
@@ -3,4 +3,5 @@ package com.radzhabov.moodtracker.presentation.theme
 import androidx.compose.ui.graphics.Color
 
 val DarkOrange = Color(0xFFFFA63C)
-val TransparentWhite = Color(0xFFFFFFFF).copy(alpha = 0.7f)
+val Sepia = Color(0xFFD19891)
+val TransparentWhite = Color(0xFFFFFFFF).copy(alpha = 0.6f)

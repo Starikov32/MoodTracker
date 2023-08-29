34a419e1441b6bcf0decd7a3d80c4e614a15f5d1
MoriatY <nacud85@gmail.com>
2023-08-29 11:09:35 +0300

    fix imports & added tests dependencies

--- app/src/main/java/com/radzhabov/moodtracker/presentation/home/animation/AnimationCard.kt
+++ app/src/main/java/com/radzhabov/moodtracker/presentation/home/animation/AnimationCard.kt
@@ -11,12 +11,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.radzhabov.moodtracker.R
